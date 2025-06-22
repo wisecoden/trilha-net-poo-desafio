e2e84371f2a47551ff43c6719a973483a3f09227
Wise <weasley.nascimento@protonmail.ch>
2025-06-22 00:38:23 -0300

    feat:implementando herança para modelos de smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,19 @@
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string IMEI;
+        private string Modelo;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            IMEI = imei;
+            Modelo = modelo;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iphone = new Iphone(numero: "21111", modelo:"modelo 1", imei: "1111111" , memoria: 256);
+
+Console.WriteLine("Smartphone Iphone: ");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 2", imei: "2222222", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WeChat");
+
